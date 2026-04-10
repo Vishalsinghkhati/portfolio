75d61d99520ece17799c6207ee9187362eac2cd1
Vishalsinghkhati <vishalkhati46@gmail.com>
2026-04-10 18:26:36 +0530

    Fix live link format in Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,7 +18,7 @@ const Projects = () => {
         "Interactive finance dashboard for tracking expenses, income analytics, and visual insights using charts and real-time data visualization.",
       tech: ["React", "Node.js", "Chart.js"],
       github: "https://github.com/Vishalsinghkhati/Finance_dashborad.git",
-      live: "finance-vishaldashborad-one.vercel.app",
+      live: "https://finance-vishaldashborad-one.vercel.app/",
     },
     {
       title: "Portfolio Website",
